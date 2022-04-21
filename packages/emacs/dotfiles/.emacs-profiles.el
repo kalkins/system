c7e5c90d974fd132e3e20cfdc42aee908a13070b
Sindre Stephansen <sindre@sindrestephansen.com>
2022-04-21 09:04:34 +0200

    Add emacs doom

--- packages/emacs/dotfiles/.emacs-profiles.el
+++ packages/emacs/dotfiles/.emacs-profiles.el
@@ -1,1 +1,2 @@
-(("default" . ((user-emacs-directory . "~/.emacs-profiles.d/custom"))))
+(("default" . ((user-emacs-directory . "~/.emacs-profiles.d/custom")))
+ ("doom" . ((user-emacs-directory . "~/.emacs-profiles.d/doom"))))
